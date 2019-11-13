75394a7d0f534b3d9a1c3a5181722a0c17d64948
POYEH <fupoya@gmail.com>
2019-11-13 08:01:06 +0800

    實驗：復原練習檔；上傳作業初稿

---
+++ Course Practice/Course _ Pratise/Course _ Pratise/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Course _ Pratise
+//
+//  Created by Po yeh Fu on 10/11/19.
+//  Copyright © 2019 Po yeh Fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    let number:Int = 10
+    let numberDescription:String
+    
+    switch number {
+    case 0: numberDe
+    
+    
+    default:    break
+    
+    
+    }
+    
+    
+    
+    
+
+}
+
